771a591b7674c41fb7e0d6018911a92de20f7ac6
Max Kukartsev <mxxk@users.noreply.github.com>
2019-07-04 16:14:32 -0700

    Update gatsby-config.js

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,5 +1,5 @@
 module.exports = {
-  pathPrefix: `test-gatsby-path-prefix-navigation`,
+  pathPrefix: `/test-gatsby-path-prefix-navigation`,
   siteMetadata: {
     title: `Gatsby Default Starter`,
     description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
